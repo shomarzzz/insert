4d6d588a2b3901574da18816a6dda99f12c4ef07
shomarzzz <67146629+shomarzzz@users.noreply.github.com>
2021-09-30 13:50:45 +0600

    Add files via upload

---
+++ jpghider.py
@@ -0,0 +1,43 @@
+class Hider:
+    def __init__(self, path, endstring = None, format=None) -> None:
+        self.path = path
+        dict = {"jpg":"FFD9", "png":"49454E44AE426082"}
+        if format is not None:
+            self.string = dict[format]
+            self.length = len(dict[format])//2
+        else:
+            self.string = endstring
+            self.length = len(endstring)//2
+
+    def appendfile(self, name):
+        with open(name, 'rb') as f:
+            bytes = f.read()
+        with open(self.path, 'ab') as file:
+            file.write(bytes)
+
+    def showoutput(self):
+        with open(self.path, 'rb') as file:
+            content = file.read()
+            index = content.index(bytes.fromhex(self.string))
+            file.seek(index+self.length)
+            byte = file.read()
+            return byte
+
+    def makefile(self, name):
+        with open(name, 'wb') as f:
+            f.write(self.showoutput())
+
+    def delete(self):
+        with open(self.path, 'rb') as file:
+            content = file.read()
+            index = content.index(bytes.fromhex(self.string))
+            content = content[:index+self.length]
+        with open(self.path, 'wb') as f:
+            f.write(content)
+    
+    def showcontent(self):
+        with open(self.path, 'rb') as file:
+            byte = file.read()
+            print(byte)
+            return byte
+    
